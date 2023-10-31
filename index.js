2373d56aaaddc626ac15b62c5fafe1b1dea2aefb
Nishant Bhalothia <nishantbhalothia444@gmail.com>
2023-10-31 17:43:16 +0530

    installed express-session to create session for user

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.set('view engine', 'ejs');
 
 // ==============================================================================================================
